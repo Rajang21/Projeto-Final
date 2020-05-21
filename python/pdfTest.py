dbb0dc5755da5de5d4df694c6afd2968d05973a1
Rajang21 <43537019+Rajang21@users.noreply.github.com>
2020-05-21 15:56:12 +0100

    Update pdfTest.py

--- python/pdfTest.py
+++ python/pdfTest.py
@@ -1,6 +1,11 @@
 # Import PyPDF2 Library
 import PyPDF2
 import re
+import os
+
+os.chdir(
+    "C:\\Users\\ASUS\\Desktop\\Semestre6\\Projeto\\Projeto1\\Projeto-Final\\python"
+)
 
 
 def pdftotext(pdfFile):
