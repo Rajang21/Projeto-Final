c172fd3ef79ad5d71d907ed02b3f05b8aa27f8a5
Rajang21 <43537019+Rajang21@users.noreply.github.com>
2020-05-26 12:43:03 +0100

    update conversor

---
+++ python/Conversor.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+import fitz  # PyMUPDF
+from pylatexenc.latexwalker import LatexWalker, LatexEnvironmentNode
+from pylatexenc.latex2text import LatexNodes2Text
+
+
+def decodePdf(path):
+    doc = fitz.open(path)
+    for page in doc:
+        text = page.getText()
+    return text
+
+
+def decodeTex(path):
+    doc = " ".join(open(path, "r").readlines())
+    doc = doc.encode("ISO-8859-1")
+    doc = doc.decode("utf-8")
+    return LatexNodes2Text().latex_to_text(doc)
+
+
+print(decodePdf("true_or_false_question.pdf"))
+print(decodeTex("answer_1_true.tex"))
--- python/helloworld.py
+++
@@ -1,5 +0,0 @@
-for i in range(10):
-    print("Hello World!!!")
-    print("Teste Edição!!!")
-    print("Teste Pull!!!")
-    print("Teste branch")
